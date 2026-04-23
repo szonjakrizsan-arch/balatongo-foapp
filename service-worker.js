04d4da232ee2c5b8bb57c6143d7b2fb6e99ed965
szonjakrizsan-arch <szonjakrizsan@gmail.com>
2026-04-23 19:31:51 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,25 @@
+const CACHE_NAME = "balatongo-v1";
+const URLS_TO_CACHE = [
+  "/balatongo-foapp/",
+  "/balatongo-foapp/index.html",
+  "/balatongo-foapp/style.css",
+  "/balatongo-foapp/script.js",
+  "/balatongo-foapp/icon-192.png",
+  "/balatongo-foapp/icon-512.png"
+];
+
 self.addEventListener("install", (event) => {
-  console.log("Service Worker telepítve");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  // egyelőre nem cache-elünk semmit
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
