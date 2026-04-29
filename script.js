100ab833475af5851ed7a8a73823b812b31eb876
szonjakrizsan-arch <szonjakrizsan@gmail.com>
2026-04-29 15:47:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   // futtatjuk azonnal + amikor a DOM kész
-  move();
+  // move();
   if (document.readyState === "loading") {
     document.addEventListener("DOMContentLoaded", move, { once: true });
   }
